a046cf7205cb88846f37ab051467268e1e5f005f
Prabin Dangol <neprabin@gmail.com>
2021-09-29 17:17:53 +0545

    Update section-about.php

--- sections/section-about.php
+++ sections/section-about.php
@@ -11,27 +11,36 @@ if (get_theme_mod('total_about_page_disable') != 'on') {
                 <?php
                 $total_about_page_id = get_theme_mod('total_about_page');
                 if ($total_about_page_id) {
-                    $args = array(
-                        'page_id' => absint($total_about_page_id)
-                    );
-                    $query = new WP_Query($args);
-                    if ($query->have_posts()):
-                        while ($query->have_posts()) : $query->the_post();
-                            ?>
-                            <h2 class="ht-section-title"><?php the_title(); ?></h2>
-                            <div class="ht-content">
-                                <?php
-                                if (has_excerpt() && '' != trim(get_the_excerpt())) {
-                                    the_excerpt();
-                                } else {
-                                    the_content();
-                                }
+                    $page_on_front = get_option('page_on_front');
+                    $page_for_posts = get_option('page_for_posts');
+
+                    if ($total_about_page_id == $page_on_front) {
+                        esc_html_e('You can not choose the page that is set as the Homepage in Settings > Reading. Please choose another Page', 'total');
+                    } else if ($total_about_page_id == $page_for_posts) {
+                        esc_html_e('You can not choose the page that is set as the Posts page. Please choose another Page in Settings > Reading', 'total');
+                    } else {
+                        $args = array(
+                            'page_id' => absint($total_about_page_id)
+                        );
+                        $query = new WP_Query($args);
+                        if ($query->have_posts()):
+                            while ($query->have_posts()) : $query->the_post();
                                 ?>
-                            </div>
-                            <?php
-                        endwhile;
-                    endif;
-                    wp_reset_postdata();
+                                <h2 class="ht-section-title"><?php the_title(); ?></h2>
+                                <div class="ht-content">
+                                    <?php
+                                    if (has_excerpt() && '' != trim(get_the_excerpt())) {
+                                        the_excerpt();
+                                    } else {
+                                        the_content();
+                                    }
+                                    ?>
+                                </div>
+                                <?php
+                            endwhile;
+                        endif;
+                        wp_reset_postdata();
+                    }
                 }
                 ?>
 
