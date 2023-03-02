68f25696e4babfe5e66b3c543af28d5cf879628b
kishan007 <hashthemesu4@gmail.com>
2023-03-02 18:04:56 +0545

    updated

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -10,12 +10,16 @@ if (!class_exists('Total_Customizer')) {
     class Total_Customizer {
 
         function __construct() {
-            require TOTAL_CUSTOMIZER_PATH . 'customizer-custom-controls.php';
-            require TOTAL_CUSTOMIZER_PATH . 'custom-controls/typography/typography.php';
-            require TOTAL_CUSTOMIZER_PATH . 'customizer-control-sanitization.php';
+            
             require TOTAL_CUSTOMIZER_PATH . 'customizer-functions.php';
-            require TOTAL_CUSTOMIZER_PATH . 'customizer-icon-manager.php';
-            require TOTAL_CUSTOMIZER_PATH . 'customizer-panel/register-customizer-controls.php';
+            require TOTAL_CUSTOMIZER_PATH . 'custom-controls/typography/typography.php';
+            if(!class_exists('TotalPlusCustomizer\Customizer')) {
+                require TOTAL_CUSTOMIZER_PATH . 'customizer-custom-controls.php';
+                require TOTAL_CUSTOMIZER_PATH . 'customizer-control-sanitization.php';
+                require TOTAL_CUSTOMIZER_PATH . 'customizer-icon-manager.php';
+                require TOTAL_CUSTOMIZER_PATH . 'customizer-panel/register-customizer-controls.php';
+            }
+
         }
 
     }
