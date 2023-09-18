c8c8bf42d163f15b697bf5dc9a27086b144f796d
Prabin Dangol <neprabin@gmail.com>
2023-09-18 18:10:57 +0545

    Update typography-settings.php

--- inc/customizer/customizer-panel/typography-settings.php
+++ inc/customizer/customizer-panel/typography-settings.php
@@ -186,6 +186,6 @@ $wp_customize->add_section(new Total_Upgrade_Section($wp_customize, 'total-hcfu-
         esc_html__('Upload custom fonts. The uploaded font will display in the typography font family list.', 'total'),
     ),
     'upgrade_text' => esc_html__('Purchase Custom Font Uploader', 'total'),
-    'upgrade_url' => 'https://hashthemes.com/checkout?edd_action=add_to_cart&download_id=48193',
+    'upgrade_url' => 'https://hashthemes.com/downloads/hash-custom-font-uploader/',
     'active_callback' => 'total_check_cfu'
 )));
