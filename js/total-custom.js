752885ce7df0120ccd3b545ab07846f61424fda0
Prabin Dangol <neprabin@gmail.com>
2024-12-05 17:35:57 +0545

    upodate

--- js/total-custom.js
+++ js/total-custom.js
@@ -91,6 +91,7 @@ jQuery(function ($) {
         delay: 500, // one second delay on mouseout
         animation: {opacity: 'show', height: 'show'}, // fade-in and slide-down animation
         speed: 'fast', // faster animation speed
+        autoArrows: false
     });
 
     $(window).resize(function () {
